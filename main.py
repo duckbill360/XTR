bab75c6abb5e31fdd0fc1513a2f963826c6a1ad0
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-06-12 16:50:39 +0800

    Completely finished.

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def Cn(n):
 
 
 if __name__ == '__main__':
-    p = int(delete_space('0ED63D1E 55533501 0A551576 3B2A9D57 DAD4CFE6 2E98A34B'), 16)
-    q = int(delete_space('0B507397 0D0FEDC5 4F45BA84 36122A02 29C0CF9F B4F6D7'), 16)
+    p = int(delete_space(input('p =')), 16)
+    q = int(delete_space(input('q =')), 16)
 
-    x = int(delete_space('05979DD0 F29B6D57 87458232 E258C35C 12898230 E4610C58'), 16)
-    y = int(delete_space('0C328613 C13A98B2 42CEF504 BBFAAE70 15C6C0F4 D3BCF162'), 16)
+    x = int(delete_space(input('Tr(g).x =')), 16)
+    y = int(delete_space(input('Tr(g).y =')), 16)
 
-    a = int(delete_space('9166EB14 BA0E8757 A9FBCF6A D13710AE A4EC4CF3 288A'), 16)
-    b = int(delete_space('09C4B2B4 734B0E8B 24119D8C 350AC19F 199FDEB2 BA5DA6'), 16)
+    a = int(delete_space(input('a =')), 16)
+    b = int(delete_space(input('a =')), 16)
 
     C = {-1: Point(x, y, p).powerP(),
          0: Point(p - 3, p - 3, p),
